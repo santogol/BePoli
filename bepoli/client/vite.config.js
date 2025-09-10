f2ed6d0a3eae59eb4cfd3248115926573c71149a
santogol <riccardosantomauro58@gmail.com>
2025-09-10 18:31:28 +0200

    Update vite.config.js

--- bepoli/client/vite.config.js
+++ bepoli/client/vite.config.js
@@ -2,12 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      // opzionale: inoltra /api al tuo Express in dev
-      '/api': 'http://localhost:3000'
-    }
-  }
+  plugins: [react()]
 })
