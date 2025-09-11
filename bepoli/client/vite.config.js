2e70c389581baf7f78d9ba0ce4acb488d1624b3b
santogol <riccardosantomauro58@gmail.com>
2025-09-11 18:07:55 +0200

    Update vite.config.js

--- bepoli/client/vite.config.js
+++ bepoli/client/vite.config.js
@@ -1,6 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000',
+      '/login': 'http://localhost:3000',
+      '/register': 'http://localhost:3000',
+      '/logout': 'http://localhost:3000',
+      '/csrf-token': 'http://localhost:3000',
+      '/auth': 'http://localhost:3000'
+    }
+  }
 })
+
