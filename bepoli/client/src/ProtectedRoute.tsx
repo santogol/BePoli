3eb2f5c1f373d13c078b60044e7b830fbc8b114b
santogol <riccardosantomauro58@gmail.com>
2025-09-15 11:02:01 +0200

    Delete bepoli/client/src/ProtectedRoute.tsx

--- bepoli/client/src/ProtectedRoute.tsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from 'react-router-dom';
-import { useAuth } from './AuthContext';
-
-export default function ProtectedRoute({children}:{children:JSX.Element}) {
-  const { user } = useAuth();
-  if (!user) return <Navigate to="/login" replace />;
-  return children;
-}
