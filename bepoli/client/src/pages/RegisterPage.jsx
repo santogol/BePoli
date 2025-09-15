9047ce77d8c574b0073aa23b667057e478a1e32b
santogol <riccardosantomauro58@gmail.com>
2025-09-15 11:43:17 +0200

    Update RegisterPage.jsx

--- bepoli/client/src/pages/RegisterPage.jsx
+++ bepoli/client/src/pages/RegisterPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import { api, ensureCsrf } from '../services/api';
+import { api } from '../services/api';
 
 export default function RegisterPage() {
   const navigate = useNavigate();
