b04fa8f9ff431c34955a3eac1452d0824fb98412
santogol <riccardosantomauro58@gmail.com>
2025-09-15 11:19:05 +0200

    Update main.jsx

--- bepoli/client/src/main.jsx
+++ bepoli/client/src/main.jsx
@@ -5,7 +5,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import App from './App';
-import './style/app.css';
+import './styles/app.css';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
@@ -14,4 +14,5 @@ createRoot(document.getElementById('root')).render(
     </AuthProvider>
   </BrowserRouter>
 );
+
 
