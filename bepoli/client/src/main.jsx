cb33ea3eb6de57e4f317b6e9e0e85ebe121fdae0
santogol <riccardosantomauro58@gmail.com>
2025-09-15 12:03:38 +0200

    Update main.jsx

--- bepoli/client/src/main.jsx
+++ bepoli/client/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import './style/app.css'   // se ce l’hai; altrimenti rimuovi questa riga
+import './styles/app.css'   // se ce l’hai; altrimenti rimuovi questa riga
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -11,6 +11,7 @@ createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </React.StrictMode>
 )
+
 
 
 
