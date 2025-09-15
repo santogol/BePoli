88377bcd01ea96c5de2c55475c0aac29a925a10f
santogol <riccardosantomauro58@gmail.com>
2025-09-15 10:49:41 +0200

    Update ProtectedRoutes.jsx

--- bepoli/client/src/routes/ProtectedRoutes.jsx
+++ bepoli/client/src/routes/ProtectedRoutes.jsx
@@ -1,1 +1,8 @@
+import { Outlet, Navigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
 
+export default function ProtectedRoutes() {
+  const { user, loading } = useAuth();
+  if (loading) return null; // puoi mettere uno spinner
+  return user ? <Outlet /> : <Navigate to="/login" replace />;
+}
